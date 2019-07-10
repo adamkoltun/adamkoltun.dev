369b888586d32ee1c9094d7e355ca8b135c475a0
Adam Koltun <akoltun4@gmail.com>
2019-07-10 13:59:07 +0200

    fix some styles at footer and nav add main js navigation and animations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// Animations
+
+sal({
+    once: false,
+})
+
+// Mobile menu & hamburger
+
 const topBar = document.querySelector('.top-bar');
 const hamburger = document.querySelector('.hamburger');
 const navMobile = document.querySelector('.mobile');
@@ -7,6 +15,31 @@ hamburger.addEventListener('click', () => {
     navMobile.classList.toggle('on');
 })
 
-sal({
-    once: false,
+// Navigation
+
+const nav = document.querySelectorAll('li a');
+
+scrollTo = element => {
+    window.scroll({
+        behavior: 'smooth',
+        left: 0,
+        top: element.offsetTop
+    });
+}
+
+closeNav = () => {
+    topBar.classList.remove('on');
+    navMobile.classList.remove('on');
+}
+
+nav.forEach(anchor => {
+    if (anchor.className === 'home') return
+    else {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const section = document.getElementById(`${this.className}`);
+            scrollTo(section);
+            closeNav();
+        })
+    }
 })
